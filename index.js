2741459ff60979b02ee9824add819b7a4e5b8cce
Volodymyr Kolchin <vkolchin@codifi.me>
2021-12-06 10:45:08 +0200

    added orders

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const exphbs = require('express-handlebars')
 const homeRoutes = require('./routes/home')
 const cardRoutes = require('./routes/card')
 const addRoutes = require('./routes/add')
+const ordersRoutes = require('./routes/orders')
 const coursesRoutes = require('./routes/courses')
+const User = require('./models/user')
 
 const app = express()
 
@@ -18,6 +20,16 @@ app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', 'views')
 
+app.use(async (req, res, next) => {
+  try {
+    const user = await User.findById('5cc1d29dcedab01481e03660')
+    req.user = user
+    next()
+  } catch (e) {
+    console.log(e)
+  }
+})
+
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({extended: true}))
 
@@ -25,6 +37,7 @@ app.use('/', homeRoutes)
 app.use('/add', addRoutes)
 app.use('/courses', coursesRoutes)
 app.use('/card', cardRoutes)
+app.use('/orders', ordersRoutes)
 
 const PORT = process.env.PORT || 3000
 
@@ -35,6 +48,15 @@ async function start() {
       useNewUrlParser: true,
       useFindAndModify: false
     })
+    const candidate = await User.findOne()
+    if (!candidate) {
+      const user = new User({
+        email: 'vladilen@mail.ru',
+        name: 'Vladilen',
+        cart: {items: []}
+      })
+      await user.save()
+    }
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`)
     })
